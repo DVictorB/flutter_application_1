2c83324509373651234f5a098cb0d70691ab1f24
DVictorB <david.balboa@tecsup.edu.pe>
2024-03-14 16:08:24 -0500

    adding files

---
+++ lib/test.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Imprimir un mensaje en la consola
+  print('¡Hola, mundo!');
+
+  // Ordenar una lista de números
+  List<int> numeros = [5, 2, 8, 1, 3];
+  numeros.sort();
+  print('Números ordenados: $numeros');
+}
